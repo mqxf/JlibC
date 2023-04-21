fd4c94b95676773051d106229c6cf124850e3d55
Maxim Savenkov <max_savenkov@mail.ru>
2023-04-21 07:41:45 +0200

    printf

--- src/main/java/dev/mv/clibs/stdio/Stdio.java
+++ src/main/java/dev/mv/clibs/stdio/Stdio.java
@@ -1,4 +1,44 @@
 package dev.mv.clibs.stdio;
 
+import dev.mv.clibs.stddef.*;
+import dev.mv.clibs.stdlib.Pointer;
+import dev.mv.clibs.stdlib.StdLib;
+import dev.mv.utils.Utils;
+
 public class Stdio {
+
+    public static void printf(String format, Object... args) {
+        char[] chars = format.toCharArray();
+        int i = 0, j = 0;
+        while (i < args.length) {
+            while ((chars[j] != '%' || chars[j + 1] == '%') && j < chars.length) j++;
+            j++;
+            if (args[i] instanceof Pointer<?>) {
+                if (chars[j] == 's') {
+                    Pointer<Char> str = (Pointer<Char>) args[i];
+                    args[i] = StdLib.toStr(str);
+                }
+                else {
+                    Pointer<?> ptr = (Pointer<?>) args[i];
+                    args[i] = ptr.deref();
+                }
+            }
+            else {
+                args[i] = Utils.<Class<? extends CType>, Object>matchReturn((Class<? extends CType>) args[i].getClass())
+                    .occasion(Char.class, ((Char) args[i]).get())
+                    .occasion(Bool.class, ((Bool) args[i]).get())
+                    .occasion(I8.class, ((I8) args[i]).get())
+                    .occasion(I16.class, ((I16) args[i]).get())
+                    .occasion(I32.class, ((I32) args[i]).get())
+                    .occasion(I64.class, ((I64) args[i]).get())
+                    .occasion(F32.class, ((F32) args[i]).get())
+                    .occasion(F64.class, ((F64) args[i]).get())
+                    .noOccasion(args[i])
+                    .value();
+            }
+            i++;
+        }
+        System.out.printf(format, args);
+    }
+
 }
